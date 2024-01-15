3f8d3eeda4302133d9a190ab81cde3df44aa830a
BlakeAnd <mblakeandrew@gmail.com>
2024-01-15 15:49:48 -0800

    got basic function triggering when zillow search is entered

--- content.js
+++ content.js
@@ -3,15 +3,24 @@ window.addEventListener('load', function () {
 
   console.log("BEEP BOOP PROGRAM ACTIVATED", zillow_search_box);
 
-  document.onkeyUp = function() {
-    let a_str = zillow_search_box.value;
+  let a_str = "";
+
+  zillow_search_box.onkeyup = function(e) {
+    // console.log("typiiiiing")
+    a_str = zillow_search_box.value;
+    console.log(a_str);
+    
+    if(zillow_search_box === document.activeElement && e.key === "Enter"){
+      alert(`entering the search ${a_str}`)
+    }
   }
 
-  // next get element and assign variable for clicking search button
-  // and handle clicking enter with search box focussed
+  let zillow_search_button = document.getElementsByClassName("StyledAdornment-c11n-8-86-1__sc-1kerx9v-0 AdornmentRight-c11n-8-86-1__sc-1kerx9v-2 ddthKA ecdnJo SearchBox__StyledSearchBoxAdornment-rxevgz-0 gXdUka")[0];
 
-
-
+  zillow_search_button.onclick = function() {
+    alert(`clicking the search ${a_str}`)
+    
+  }
 
   // document.onclick = function(){
   //   console.log("something WAS clicked!");

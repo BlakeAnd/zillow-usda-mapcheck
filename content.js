e40a98a56b793d8661f58a1482ca35d4c8029f52
BlakeAnd <mblakeandrew@gmail.com>
2024-02-17 13:43:09 -0800

    pulled address from zillow page and formatted into js array of objects

--- content.js
+++ content.js
@@ -1,21 +1,80 @@
 window.addEventListener('load', function () {
-  let zillow_search_box = document.getElementsByClassName("StyledFormControl-c11n-8-86-1__sc-18qgis1-0 DA-dAx Input-c11n-8-86-1__sc-4ry0fw-0 hxjfkY")[0];
+  // let zillow_search_box = document.getElementsByClassName("StyledFormControl-c11n-8-86-1__sc-18qgis1-0 DA-dAx Input-c11n-8-86-1__sc-4ry0fw-0 hxjfkY")[0];
 
-  console.log("BEEP BOOP PROGRAM ACTIVATED", zillow_search_box);
 
-  let search_str = "";
+  // Select the specific <ul>
+  const propertiesList = document.querySelector('.List-c11n-8-84-3__sc-1smrmqp-0'); // StyledSearchListWrapper-srp__sc-1ieen0c-0 doa-doM gKnRas photo-cards photo-cards_extra-attribution
 
-  // zillow_search_box.onkeyup = function(e) {
-  //   // console.log("typiiiiing")
-  //   search_str = zillow_search_box.value;
-  //   console.log(search_str);
+  console.log("BEEP BOOP PROGRAM ACTIVATED", propertiesList);
+
+
+  // Select all <li> elements within this <ul>
+  const listItems = document.getElementsByTagName('address');
+  let addresses_arr = setAddressesArray();
+
+  function setAddressesArray(){
+    let returned_addresses_array = [];
+
+    for(let i = 0; i < listItems.length; i++){
+      let addresses_obj = {
+        address: "",
+        address_url: ""
+      }
+      addresses_obj.address = listItems[i].innerHTML;
+      addresses_obj.address_url = addresses_obj.address.replace(/ /g, "%20");
+      returned_addresses_array.push(addresses_obj);
+    }
+    return returned_addresses_array;
+  }
+
+  console.log("addresses", listItems, addresses_arr)
+  // Loop through the <li> elements using a for loop
+  for(let i = 0; i < listItems.length; i++) {
+      console.log("woot", i, listItems[i].innerHTML); // Or any operation you want to perform
+  }
+
+
+  function throttle(func, limit) {
+    let lastFunc;
+    let lastRan;
+    return function() {
+        const context = this;
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+}
+
+
+const throttledScroll = throttle(function() {
+    console.log('Throttled scroll event triggered', listItems);
+    let old_array_length = addresses_arr.length;
+    addresses_arr = setAddressesArray();
+    let newly_loaded_arr = addresses_arr.slice(old_array_length)
     
-  //   if(zillow_search_box === document.activeElement && e.key === "Enter"){
-  //     alert(`entering the search ${search_str}`)
-  //   }
-  // }
+}, 200); // Trigger at most once every 200ms (1/5th of a second)
 
-  let zillow_search_button = document.getElementsByClassName("StyledAdornment-c11n-8-86-1__sc-1kerx9v-0 AdornmentRight-c11n-8-86-1__sc-1kerx9v-2 ddthKA ecdnJo SearchBox__StyledSearchBoxAdornment-rxevgz-0 gXdUka")[0];
+// window.addEventListener('scroll', throttledScroll);
+
+  const scrollArea = document.querySelector('#search-page-list-container')
+  console.log("where it scrolls", scrollArea)
+
+  scrollArea.addEventListener('scroll', throttledScroll);
+  // function() {
+  //   // Your function code here
+  //   console.log('Properties being scrolled');
+  // });
+ 
 
 
   let url_start = "https://eligibility.sc.egov.usda.gov/eligibility/MapAddressVerification?address="
@@ -24,14 +83,14 @@ window.addEventListener('load', function () {
   // zillow_search_button.onclick 
   check_eligibility();
   function check_eligibility() {
-    let url_search_str = search_str.replaceAll(" ", "%20");
+    // let url_search_str = search_str.replaceAll(" ", "%20");
 
-    // URL of the page you want to fetch
-    url_search_str = "370%20boyer%20rd"
-    const url = url_start + url_search_str + url_end;
+    // // URL of the page you want to fetch
+    // url_search_str = "370%20boyer%20rd"
+    // const url = url_start + url_search_str + url_end;
 
-    console.log(url)
-    // alert(`clicking the search ${url}`)
+    // console.log(url)
+    // // alert(`clicking the search ${url}`)
 
     let address = "370%20boyer%20rd"; // Example, get this from your page interactions
     let eligibility_response = "";
@@ -45,56 +104,11 @@ window.addEventListener('load', function () {
       // alert(response.data);
     });
 
-    // Use fetch to get the content
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const targetUrl = "https://eligibility.sc.egov.usda.gov/eligibility/MapAddressVerification?address=370%20boyer%20rd&whichapp=RBSIELG"; // The URL you want to fetch
-  // fetch(`${proxyUrl}${targetUrl}`)
-  // .then(response => response.text())
-  // .then(data => console.log(data))
-  // .catch(error => console.error('Error:', error));
-    // fetch(`${targetUrl}`)
-    //   .then(response => {
-    //     // Check if the request was successful
-    //     if (!response.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-    //     // Return the text content of the response
-    //     return response.text();
-    //   })
-    //   .then(data => {
-    //     // 'data' contains the content of the webpage
-    //     alert(data)
-    //     console.log(data);
-    //   })
-    //   .catch(error => {
-    //     // Handle any errors that occurred during the fetch
-    //     alert(error)
-    //     console.error('Fetch error:', error);
-    //   });
+
+
 
       
 
   }
-
-  // document.onclick = function(){
-  //   console.log("something WAS clicked!");
-  //   console.log(zillow_search_box);
-  //   console.log("ELLO!");
-  //   console.log(document.activeElement);
-
-  //   if (zillow_search_box === document.activeElement) {
-      
-  //     console.log("FOCUSSED")
-  //     document.onkeypress = function() {
-  //       let str = document.activeElement.value;
-  //       console.log(str)
-  //     }
-  //   }
-  //   else{
-  //     console.log("no focus!")
-  //   }
-
-  // };
-
 
 })
